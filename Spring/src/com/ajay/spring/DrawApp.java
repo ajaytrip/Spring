605e72c690ca8dfacaddd958f19142f3f721a597
aj <ajaytrip@gmail.com>
2017-02-07 05:12:48 +0530

    test first commit

---
+++ Spring/src/com/ajay/spring/DrawApp.java
@@ -0,0 +1,23 @@
+package com.ajay.spring;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class DrawApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//Triangle triangle = new Triangle();
+		
+		//BeanFactory factory = new XmlBeanFactory (new FileSystemResource("spring.xml"));
+		ApplicationContext context= new ClassPathXmlApplicationContext("Spring.xml");
+		Triangle obj = (Triangle) context.getBean("triangle");
+		obj.draw();
+
+	}
+
+}
